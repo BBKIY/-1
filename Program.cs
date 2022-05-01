71c8ff476578306a327f35f2fc5d3a512d44d0f8
BBKIY <104115351+BBKIY@users.noreply.github.com>
2022-05-01 15:46:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte age = 25;
+            sbyte money = 100;
+            short distance = 250;
+            ushort time = 4;
+            int hitPoints = 100;
+            uint level = 45;
+            long rating = 200;
+            ulong pointsCount = 450;
+            string name = "Стас";
+            bool isHealth;
+
+        }
+    }
+}
