941ce5c557aca0cfa08cb9637dd25856f69cc6e3
BBKIY <104115351+BBKIY@users.noreply.github.com>
2023-04-20 20:19:13 +0300

    123

--- Program.cs
+++ Program.cs
@@ -3,24 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace циклы
+/*
+    Написать программу, которая будет выполняться до тех пор, пока не будет введено слово exit.
+    Помните, в цикле должно быть условие, которое отвечает за то, когда цикл должен завершиться.
+    Это нужно, чтобы любой разработчик взглянув на ваш код, понял четкие границы вашего цикла.
+ */
+
+namespace ConsoleApp1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            string a;
-            a = Console.ReadLine();
+            bool commandStartProgramm = true;
 
-            while (true)
+            while (commandStartProgramm)
             {
-                Console.WriteLine("не верно");
-                if (a == "exit") 
+                Console.WriteLine("Введите команду(/help - для получения справки.): ");
+                string command = Console.ReadLine().ToLower();
+                switch (command)
                 {
-                    break;
+                    case "/help":
+                        Console.WriteLine("/hacked - взломать пентагон. \n/offworldinternet - отключить всемирный интернет. \n/reloadworld - перезапуск земли. \n/clear - очистка окна. \n/exit - выйти из программы.");
+                        break;
+                    case "/hacked":
+                        Console.WriteLine("Пентагон взломан все необходимые данные вы можите найти в очищеной корзине =)");
+                        break;
+                    case "/offworldinternet":
+                        Console.WriteLine("Интернет отключен во всем мире. Не возможно соедениться с сервером для передачи команд...");
+                        break;
+                    case "/reloadworld":
+                        Console.WriteLine("Мир перезагружен все началось с начала. Соответственно вас больше не существет досвидания!");
+                        Console.WriteLine("Ваш компьютер самоуничтожится по нажатии любой клавиши =D");
+                        Console.ReadKey();
+                        break;
+                    case "/clear":
+                        Console.Clear();
+                        Console.WriteLine("Окно приложения очищено.");
+                        break;
+                    case "/exit":
+                        commandStartProgramm = false;
+                        break;
+                    default:
+                        Console.WriteLine("Вы ввели не верную команду. Воспользуйтесь справкой(/help)");
+                        break;
                 }
             }
+            Console.WriteLine("Для выхода нажмите любую клавишу...");
+            Console.ReadKey();
         }
     }
 }
